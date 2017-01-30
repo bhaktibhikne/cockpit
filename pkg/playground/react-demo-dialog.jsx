f893ae57fc41dd2b75dcca46e098139dcfa16c36
Bhakti Bhikne <bhaktibhikne@users.noreply.github.com>
2017-01-30 20:42:24 +0530

    Update react-demo-dialog.jsx

--- pkg/playground/react-demo-dialog.jsx
+++ pkg/playground/react-demo-dialog.jsx
@@ -107,3 +107,68 @@
 
     module.exports = PatternDialogBody;
 }());
+
+// Add Port Dialog Box
+(function() {
+    "use strict";
+
+    var cockpit = require("cockpit");
+
+    var React = require("react");
+    var Select = require("cockpit-components-select.jsx");
+
+    var _ = cockpit.gettext;
+
+   
+    var PatternDialogBody = React.createClass({
+        selectChanged: function(value) {
+            console.log("new value: " + value);
+        },
+        render: function() {
+            return (
+                <div className="modal-body">
+                    <table className="form-table-ct">
+                        <tr>
+                            <td className="top">
+                                <label className="control-label" for="control-1">
+                                    Port Number
+                                </label>
+                            </td>
+                            <td>
+                                <input id="control-1" className="form-control" type="text"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="top">
+                                <label className="control-label">
+                                  Description  
+                                </label>
+                            </td>
+                            <td>
+                                <input id="control-1" className="form-control" type="text"/>
+                              
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="top">
+                                <label className="control-label">
+                                    Protocol
+                                </label>
+                            </td>
+                            <td>
+                                <Select.Select initial="One">
+                                    <Select.SelectEntry data="One">TCP</Select.SelectEntry>
+                                    <Select.SelectEntry data="Two">UDP</Select.SelectEntry>
+                                    <Select.SelectEntry data="Three">ICMP</Select.SelectEntry>
+                                </Select.Select>
+                            </td>
+                        </tr>
+                       
+                    </table>
+                </div>
+            );
+        }
+    });
+
+    module.exports = PatternDialogBody;
+}());
